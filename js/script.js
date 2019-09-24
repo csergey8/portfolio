89ebf2310e14faaf81bbcefc1de1c772b8560c39
csergey8 <c.sergey@protonmail.com>
2019-09-24 11:32:54 +0300

    some styles and phone show script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const numberText = document.getElementById('number');
   
-  console.log('hi');
+  numberText.addEventListener('click', () => {
+    numberText.textContent = '+38(093)704-28-44';
+  })
 });
 
 var rellax = new Rellax('.rellax');
